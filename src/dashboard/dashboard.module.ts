b86273a46ee8f3d8dfff13d39e09103f1678ccab
Kian Janloo <ddrdslayer@gmail.com>
2025-07-15 16:11:32 +0330

    Summary

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from 'src/entities/products.entity';
 import { User } from 'src/entities/users.entity';
 import { Favorite } from 'src/entities/favorites.entity';
+import { Message } from 'src/entities/messages.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, User, Favorite])],
+  imports: [TypeOrmModule.forFeature([Product, User, Favorite, Message])],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Favorite } from 'src/entities/favorites.entity';
+import { Message } from 'src/entities/messages.entity';
 import { Product } from 'src/entities/products.entity';
 import { User } from 'src/entities/users.entity';
 import { Repository } from 'typeorm';
@@ -14,13 +15,16 @@ export class DashboardService {
     private readonly productRepository: Repository<Product>,
     @InjectRepository(Favorite)
     private readonly favoriteRepository: Repository<Favorite>,
+    @InjectRepository(Message)
+    private readonly messageRepository: Repository<Message>,
   ) {}
 
   getSummary = async () => {
     const totalUsers = await this.userRepository.count();
     const totalProducts = await this.productRepository.count();
     const totalFavorites = await this.favoriteRepository.count();
+    const totalMessages = await this.messageRepository.count();
 
-    return { totalUsers, totalProducts, totalFavorites };
+    return { totalUsers, totalProducts, totalFavorites, totalMessages };
   };
 }
