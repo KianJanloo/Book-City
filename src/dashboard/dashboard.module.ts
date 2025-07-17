46d8e70e2785a0bac0fbd16d11e21ba765e2816b
Kian Janloo <ddrdslayer@gmail.com>
2025-07-17 21:31:51 +0330

    Summary

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -6,9 +6,20 @@ import { Product } from 'src/entities/products.entity';
 import { User } from 'src/entities/users.entity';
 import { Favorite } from 'src/entities/favorites.entity';
 import { Message } from 'src/entities/messages.entity';
+import { Payment } from 'src/entities/payments.entity';
+import { Order } from 'src/entities/orders.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, User, Favorite, Message])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Product,
+      User,
+      Favorite,
+      Message,
+      Payment,
+      Order,
+    ]),
+  ],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Favorite } from 'src/entities/favorites.entity';
 import { Message } from 'src/entities/messages.entity';
+import { Order } from 'src/entities/orders.entity';
+import { Payment } from 'src/entities/payments.entity';
 import { Product } from 'src/entities/products.entity';
 import { User } from 'src/entities/users.entity';
 import { Repository } from 'typeorm';
@@ -17,6 +19,10 @@ export class DashboardService {
     private readonly favoriteRepository: Repository<Favorite>,
     @InjectRepository(Message)
     private readonly messageRepository: Repository<Message>,
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>,
   ) {}
 
   getSummary = async () => {
@@ -24,7 +30,16 @@ export class DashboardService {
     const totalProducts = await this.productRepository.count();
     const totalFavorites = await this.favoriteRepository.count();
     const totalMessages = await this.messageRepository.count();
+    const totalPayments = await this.paymentRepository.count();
+    const totalOrders = await this.orderRepository.count();
 
-    return { totalUsers, totalProducts, totalFavorites, totalMessages };
+    return {
+      totalUsers,
+      totalProducts,
+      totalFavorites,
+      totalMessages,
+      totalPayments,
+      totalOrders,
+    };
   };
 }
